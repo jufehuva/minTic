85b9c49ae6152193591994c5b6bae25873b67853
unknown <invitadodesarrolloeficaz@hotmail.com>
2021-11-17 17:05:56 -0500

    src

---
+++ js/src.js
@@ -0,0 +1,33 @@
+let operaciones = [];
+let balance = 0;
+function parametros(params) {
+    
+}
+
+const registrarOperacion = (monto, tipo, categoria, fecha)=>{
+    console.log(operaciones); 
+    valMonto = parseInt(monto);
+
+    console.log(valMonto);
+    console.log(tipo);
+    console.log(categoria);
+    console.log(fecha);
+
+    operaciones.push({
+        montoVal:console.log(operaciones),
+        tipoVal: tipo,
+        categoriaVal: categoria,
+        fechaVal: fecha
+    });
+}
+
+console.log(operaciones);
+
+
+const registrarMetaAhorro=(meta)=>{
+
+}
+
+const estaMiMetaCumplida=()=>{
+
+}
