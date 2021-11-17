a53d545492fff3457ca235eb291b8538b30f7fd9
jufehuva <91357991+jufehuva@users.noreply.github.com>
2021-11-17 16:35:46 -0500

    Add files via upload

---
+++ js/src.js
@@ -0,0 +1,19 @@
+let operaciones=[];
+let balance=0;
+
+const registrarOperacion=(monto,tipo,categoria,fecha)=>{
+    operaciones.push(
+        {
+            monto: x,
+            tipo: gasto-ingreso,
+            categoria: x,
+            fecha: date
+        }
+    );
+}
+const registrarMetaAhorro=(meta)=>{}
+const estaMiMetaCumplida=()=>{}
+
+module.exports.registrarMetaAhorro=registrarMetaAhorro;
+module.exports.estaMiMetaCumplida=estaMiMetaCumplida;
+module.exports.registrarOperacion=registrarOperacion;
